e526c48ef392ac40de965c750d74ad540d591898
Warren Fowler <warrenf0122@gmail.com>
2024-09-08 19:16:55 -0400

    Scroll through questions
    To make it usable with an input box, all I have to do is make an input box, and replace the *Next* button with a *Submit*

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+
+
+
 function QuestionSetGenerator(op, max_int, num_questions) {
 
     class Question {
@@ -35,62 +38,100 @@ function QuestionSetGenerator(op, max_int, num_questions) {
     return question_array;
 }
 
-function AskQuestions(list, practice = false) {
-    let correct = 0;
-    let incorrect = [];
+// function AskQuestions(list, practice = false) {
+//     let correct = 0;
+//     let incorrect = [];
 
-    if (practice == false) {
-        for (let i = 0; i < list.length; i++) { // Fixed `length(list)` to `list.length`
-            let current_question = list[i];
+//     if (practice == false) {
+//         for (let i = 0; i < list.length; i++) { // Fixed `length(list)` to `list.length`
+//             let current_question = list[i];
 
-            let user_answer = prompt(`What is ${current_question.string}?`, 0);
+//             let user_answer = prompt(`What is ${current_question.string}?`, 0);
 
-            if (user_answer == current_question.answer) {
-                correct += 1;
-            } else {
-                incorrect.push(list[i]);
-            }
-        }
-    } else {
-        while (list.length != 0) { // Fixed `length(list)` to `list.length`
+//             if (user_answer == current_question.answer) {
+//                 correct += 1;
+//             } else {
+//                 incorrect.push(list[i]);
+//             }
+//         }
+//     } else {
+//         while (list.length != 0) { // Fixed `length(list)` to `list.length`
 
-            let random_number = Math.floor(Math.random() * list.length);
-            let current_question = list[random_number]; // Moved this line before `prompt`
+//             let random_number = Math.floor(Math.random() * list.length);
+//             let current_question = list[random_number]; // Moved this line before `prompt`
 
-            let user_answer = prompt(`What is ${current_question.string}?`, 0);
+//             let user_answer = prompt(`What is ${current_question.string}?`, 0);
 
-            if (user_answer == current_question.answer) {
+//             if (user_answer == current_question.answer) {
 
-                alert("Correct!");
+//                 alert("Correct!");
 
-                list.splice(list.indexOf(current_question), 1); // Added 1 to splice correctly
+//                 list.splice(list.indexOf(current_question), 1); // Added 1 to splice correctly
 
-            } else {
-                alert(`Incorrect. The correct answer is ${current_question.answer}.`);
+//             } else {
+//                 alert(`Incorrect. The correct answer is ${current_question.answer}.`);
 
-                current_question.attempts += 1;
+//                 current_question.attempts += 1;
 
-                if (current_question.attempts == 3) {
-                    alert("You got this wrong too many times.");
+//                 if (current_question.attempts == 3) {
+//                     alert("You got this wrong too many times.");
 
-                    list.splice(list.indexOf(current_question), 1); // Added 1 to splice correctly
-                }
-            }
-        }
+//                     list.splice(list.indexOf(current_question), 1); // Added 1 to splice correctly
+//                 }
+//             }
+//         }
+//     }
+//     return [incorrect, correct]; // Changed return format to array
+// }
+
+
+let num_of_questions = 5
+let max_int = 10
+
+// Generate a starter list
+let question_list = QuestionSetGenerator("add", num_of_questions, max_int)
+
+// Sets it to show first question
+let question_num = 0;
+
+// Puts the question and question number on the screen
+function showQuestion() {
+    document.getElementById("question").innerText = question_list[question_num].string;
+    document.getElementById("question_num").innerText = `Question ${question_num + 1} of ${question_list.length}`;
+}
+showQuestion()
+
+// Goes back a question (if not question 1), and updates screen
+function backQuestion() {
+    if (question_num != 0) {
+        question_num -= 1;
     }
-    return [incorrect, correct]; // Changed return format to array
+    showQuestion()
 }
 
-let add_questions = QuestionSetGenerator("add", 5, 10);
-let question_list = JSON.parse(JSON.stringify(add_questions));
-
-let [incorrectly_answered, num_correct] = AskQuestions(question_list);
-
-if (incorrectly_answered.length != 0) { // Fixed `length(incorrectly_answered != 0)` to `incorrectly_answered.length != 0`
-
-    let ask_practice = prompt("Would you like to practice the questions you got incorrect? [y/n]", "n").toLowerCase();
-
-    if (ask_practice == "y") {
-        AskQuestions(incorrectly_answered, practice = true);
+// Goes forward a question (if not last question), and updates screen
+function nextQuestion() {
+    if (question_num != question_list.length - 1) {
+        question_num += 1;
     }
+    showQuestion()
 }
+
+// Regenerates a list, resets screen
+function newList() {
+    question_list = QuestionSetGenerator("add", num_of_questions, max_int)
+    question_num = 0
+    showQuestion()
+}
+
+
+// let [incorrectly_answered, num_correct] = AskQuestions(question_list);
+
+// if (incorrectly_answered.length != 0) { // Fixed `length(incorrectly_answered != 0)` to `incorrectly_answered.length != 0`
+
+//     let ask_practice = prompt("Would you like to practice the questions you got incorrect? [y/n]", "n").toLowerCase();
+
+//     if (ask_practice == "y") {
+//         AskQuestions(incorrectly_answered, practice = true);
+//     }
+// }
